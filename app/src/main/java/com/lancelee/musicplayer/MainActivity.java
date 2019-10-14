a50abd195c74fb2e2c349a0971e266f66db63d7c
LanceLee-ss <hahalesss@yahoo.com>
2019-10-14 14:43:25 +0800

    1.更新

--- app/src/main/java/com/lancelee/musicplayer/MainActivity.java
+++ app/src/main/java/com/lancelee/musicplayer/MainActivity.java
@@ -7,10 +7,27 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.ActivityManager;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.text.TextUtils.lastIndexOf;
+
+public class MainActivity<isMusicPlayerInit> extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +44,9 @@ public class MainActivity extends AppCompatActivity {
     private static final int PERMISSIONS_COUNT = 1;
 
     @SuppressLint("NewApi")
-    private boolean arePermissionDenied(){
-        for(int i = 0;i < PERMISSIONS_COUNT ; i++){
-            if(checkSelfPermission(PERMISSIONS[i]) !=PackageManager.PERMISSION_GRANTED){
+    private boolean arePermissionDenied() {
+        for (int i = 0; i < PERMISSIONS_COUNT; i++) {
+            if (checkSelfPermission(PERMISSIONS[i]) != PackageManager.PERMISSION_GRANTED) {
                 return true;
             }
         }
@@ -38,24 +55,176 @@ public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("NewApi")
     @Override
-    public void onRequestPermissionsResult(int requestCode,String[] permission,int[] grantResults){
-        super .onRequestPermissionsResult(requestCode,permission,grantResults);
-        if(arePermissionDenied()){
-            ((ActivityManager)(this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
-        recreate();
-        }else{
-        onResume();
+    public void onRequestPermissionsResult(int requestCode, String[] permission, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permission, grantResults);
+        if (arePermissionDenied()) {
+            ((ActivityManager) (this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
+            recreate();
+        } else {
+            onResume();
         }
     }
 
     private boolean isMusicPlayerInit;
 
-    @Override
-    protected void onResume(){
-        super.onResume();
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && arePermissionDenied()){
-                requestPermissions(PERMISSIONS,REQUEST_PERMISSIONS);
-            }
+    private List<String> musicFilesList;
+
+    private void addMusicFilesFrom(String dirPath) {
+        final File musicDir = new File(dirPath);
+        if (!musicDir.exists()) {
+            musicDir.mkdir();
             return;
         }
+        final File[] files = musicDir.listFiles();
+        for (File file : files) {
+            final String path = file.getAbsolutePath();
+            if (path.endsWith(".mp3")) {
+                musicFilesList.add(path);
+            }
+        }
+    }
+
+    private void fillMusicList() {
+        musicFilesList.clear();
+        addMusicFilesFrom(String.valueOf(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_MUSIC)));
+        addMusicFilesFrom(String.valueOf(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOWNLOADS)));
+    }
+
+    private MediaPlayer mp;
+
+    private int playMusicFile(String path){
+        mp = new MediaPlayer();
+        try{
+            mp.setDataSource(path);
+            mp.prepare();
+            mp.start();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return mp.getDuration();
+    }
+
+    private int songPosition;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && arePermissionDenied()) {
+            requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
+            return;
+        }
+
+        if (!isMusicPlayerInit) {
+            final ListView listView = findViewById(R.id.listview);
+            final TextAdapter textAdapter = new TextAdapter();
+            musicFilesList = new ArrayList<>();
+            fillMusicList();
+            textAdapter.setData(musicFilesList);
+            listView.setAdapter(textAdapter);
+            final SeekBar seekBar = findViewById(R.id.seekBar);
+
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                int songProgress;
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    songProgress = progress;
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+
+                }
+
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    mp.seekTo(songProgress);
+                }
+            });
+
+            final TextView songPositionTextView = findViewById(R.id.currentPosition);
+            final TextView songDurationTextiew = findViewById(R.id.songDuration);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                @Override
+                public void onItemClick(AdapterView<?> parent,View view,int position,long id){
+                    final String musicFilePath = musicFilesList.get(position);
+                    final int songDuration = playMusicFile(musicFilePath)/1000;
+                    seekBar.setMax(songDuration);
+                    seekBar.setVisibility(View.VISIBLE);
+                    songDurationTextiew.setText(String.valueOf(songDuration/60+":"+String.valueOf(songDuration%60)));
+                    new Thread(){
+                        public void run(){
+                            songPosition = 0;
+                            while(songPosition<songDuration){
+                                try {
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                songPosition++;
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        seekBar.setProgress(songPosition);
+                                        songPositionTextView.setText(String.valueOf(songPosition));
+                                    }
+                                });
+                            }
+                        }
+                    }.start();
+
+                }
+            });
+
+            isMusicPlayerInit = true;
+        }
+    }
+
+    class TextAdapter extends BaseAdapter {
+
+        private List<String> data = new ArrayList<>();
+
+        void setData(List<String> mData) {
+            data.clear();
+            data.addAll(mData);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public String getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                convertView = LayoutInflater.from(parent.getContext()).
+                        inflate(R.layout.item, parent, false);
+                convertView.setTag(new ViewHolder((TextView) convertView.findViewById(R.id.myItem)));
+            }
+            ViewHolder holder = (ViewHolder) convertView.getTag();
+            final String item = data.get(position);
+            holder.info.setText(item.substring(item.lastIndexOf("/") + 1));
+            return convertView;
+        }
+
+        class ViewHolder {
+            TextView info;
+
+            ViewHolder(TextView mInfo) {
+                info = mInfo;
+            }
+        }
+    }
 }
